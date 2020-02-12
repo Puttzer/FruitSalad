3155b0de11568d588db1e683985436031299ca93
Puttzer <53831562+Puttzer@users.noreply.github.com>
2020-02-12 11:27:14 +0100

    Fruitsalad V1 setp 1 and 2 complete

---
+++ App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <p v-on:click="changeText">{{message}}</p>
+    <ul id="FRUKTER">
+      <li v-for="item in Frukter" v-bind:key="item" v-on:click="moveFrukt(item)">{{item}}</li>
+    </ul>
+
+    <ul id="SALLAD">
+      <li v-for="item in Sallad" v-bind:key="item">{{item}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    message: "Hello Vue!",
+    index: 0,
+    Frukter: ["Banan", "kiwi", "Päron"],
+    Sallad: []
+  }),
+
+  methods: {
+    changeText() {
+      this.message = "Grillkorv";
+    },
+
+    moveFrukt(fruit) {
+      console.log(fruit);
+      this.Sallad.push(fruit);
+      let index = this.Frukter.indexOf(fruit);
+      this.Frukter.splice(index, 1);
+      // Find index of fruit in this.Frukter
+      // Splice the shit out of that index
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  background-color: hotpink;
+}
+</style>
